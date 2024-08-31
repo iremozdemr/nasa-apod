1038b1e8da3df6fa96b9bf07e29552b7ad5e2615
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-08-31 23:50:51 +0300

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
+import { useState, useEffect } from 'react';
+import Header from './Header';
+
 function App() {
-  
+  //state'leri tanımlama
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  //api'den veri çekme işlemi
+  const fetchData = async () => {
+    try{
+      const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+
+      if(response.ok == false) {
+        throw new Error('veri çekme işlemi basarisiz oldu');
+      }
+      const data = await response.json();
+      setData(data);
+    } 
+    catch(err){
+      setError(err.message);
+    } 
+    finally{
+      setLoading(false);
+    }
+  };
+  //async: 
+  //Promise döndürür
+  //asenkron işlemler belirli bir süre alabilecek işlemlerdir
+  //await:
+  //Promise döndürür
+  //js Promise'in tamamlanmasını bekler
+  //kodun sıralı şekilde çalışması sağlanır
+
+  //api'den veri çekme işlemi
+  useEffect(() => {fetchData();},[]); 
+
+  //yüklenme durumu
+  if(loading){
+    return <div>yükleniyor...</div>;
+  }
+
+  //hata durumu
+  if(error){
+    return <div>hata: {error}</div>;
+  }
+
+  //veri gösterimi
+  return (
+    <>
+      <Header/>
+      <div className='container'>
+        <p className='image-explanation'>{data.explanation}</p>
+        <img className='image' src={data.url} alt={data.title}/>
+        <p className='image-title'>{data.title}</p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div className="header">
+            <img className="header-logo" src="nasa-logo.png" alt="nasa logo"/>
+            <h1 className="header-title">astronomy picture of the day</h1>
+        </div>
+    );
+}
+
+export default Header
